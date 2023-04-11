72fc2e6d5b5c801c980e9a9c3e7ad66f5fd39017
LauritsNoergaard <laur8709@stud.kea.dk>
2023-04-11 13:22:41 +0200

    UserRepo Register and login added

--- src/main/java/com/example/oenskeliste/Repository/UserRepo.java
+++ src/main/java/com/example/oenskeliste/Repository/UserRepo.java
@@ -1,8 +1,34 @@
 package com.example.oenskeliste.Repository;
 
 
+import com.example.oenskeliste.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepo {
+
+    @Autowired
+    JdbcTemplate template;
+    public void register(User user) {
+        String sql = "INSERT INTO wishlist.user (user_id, user_name, password) " +
+                "VALUES (?, ?, ?)";
+        template.update(sql, user.getUser_id(), user.getUser_name(), user.getPassword());
+    }
+
+    public boolean login(User user) {
+        String sql = "SELECT * FROM wishlist.user WHERE user_name = ? AND password = ?";
+        RowMapper<User> rm = new BeanPropertyRowMapper<>(User.class);
+        try{
+            User u = template.queryForObject(sql, rm, user.getUser_name(), user.getPassword());
+            return true;
+        } catch (EmptyResultDataAccessException e) {
+            return false;
+        }
+    }
+
 }
